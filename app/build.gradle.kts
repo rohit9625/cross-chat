766e513227a3e821238dc1d5f6c9fb61ffcc71a4
Rohit Verma <rv17837@outlook.com>
2026-02-04 12:26:08 +0530

    fix: use correct slf4j version and OkHTTP engine for ktor
    Android engine doesn't support web sockets that we need for chat functionality

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 
 dependencies {
     implementation(libs.ktor.client.core)
-    implementation(libs.ktor.client.android)
+    implementation(libs.ktor.client.okhttp)
     implementation(libs.ktor.client.content.negotiation)
     implementation(libs.ktor.serialization.kotlinx.json)
     implementation(libs.ktor.client.logging)
