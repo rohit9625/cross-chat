971038fa93a55f052f915be801742fbc08294675
Rohit Verma <rv17837@outlook.com>
2026-02-04 22:38:12 +0530

    fix: make AccessCredential serializable to avoid crash when saving token

--- app/src/main/java/dev/androhit/crosschat/domain/model/AccessCredentials.kt
+++ app/src/main/java/dev/androhit/crosschat/domain/model/AccessCredentials.kt
@@ -1,5 +1,8 @@
 package dev.androhit.crosschat.domain.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class AccessCredentials(
     val refreshToken: String? = null,
     val accessToken: String? = null
