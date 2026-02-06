a4a7f583725158ceb622a210abe5a89e2e796b58
Rohit Verma <rv17837@outlook.com>
2026-02-06 14:58:20 +0530

    feat: create chat list screen and UI states

---
+++ app/src/main/java/dev/androhit/crosschat/chat/ui/states/ChatUiState.kt
@@ -0,0 +1,20 @@
+package dev.androhit.crosschat.chat.ui.states
+
+import dev.androhit.crosschat.chat.domain.model.Chat
+import dev.androhit.crosschat.util.DateTimeUtils
+
+data class ChatUiState(
+    val id: Int,
+    val title: String,
+    val subTitle: String,
+    val lastMessageTime: String?,
+)
+
+fun Chat.toUiState() = ChatUiState(
+    id = id,
+    title = displayName,
+    subTitle = lastMessage?.let {
+        "${lastMessage.senderName.split(" ").first()}: ${lastMessage.text}"
+    } ?: "Your chat with $displayName",
+    lastMessageTime = DateTimeUtils.formatTime(lastMessageTime),
+)
